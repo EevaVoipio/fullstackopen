0db9bb38eda299540727858a372d37d4bcd8e0f7
Eeva Sasaki <eeva.sasaki@relexsolutions.com>
2020-06-27 22:07:23 +0300

    Initial commit with exercises 1.1-1.4 done

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exerciseCount}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return props.parts.map((part) => (
+    <Part name={part.name} exerciseCount={part.exercises} />
+  ));
+};
+
+const Total = (props) => {
+  return props.parts.reduce((part1, part2) => part1 + part2.exercises, 0);
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+  const parts = [part1, part2, part3];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
