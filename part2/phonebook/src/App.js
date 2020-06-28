668307944faafa1e8693fc36b2c68aeff345d397
Eeva Sasaki <eeva.sasaki@relexsolutions.com>
2020-06-28 23:35:24 +0300

    2.6-2.9

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchName, setSearchName] = useState("");
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchName = (event) => {
+    setSearchName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={searchName} onChange={handleSearchName} />
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons
+        .filter((person) => person.name.includes(searchName))
+        .map((person) => (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default App;
