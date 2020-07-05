ff2e39f35b9a88a986105a75d7e8844da2db31c3
Eeva Sasaki <eeva.sasaki@relexsolutions.com>
2020-07-05 14:07:39 +0300

    4.6-4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -11,16 +11,54 @@ const favoriteBlog = (blogs) => {
     return { 'title': '',
       'author':'',
       'likes':'' }
-  } else {
-    const maxLikes =  Math.max.apply(Math, blogs.map(function(blog){return blog.likes}))
-    const maxBlogObject =  blogs.find(blog => blog.likes === maxLikes)
-    const favouriteBlog = { 'title': maxBlogObject.title,
-      'author':maxBlogObject.author,
-      'likes':maxBlogObject.likes }
-    return favouriteBlog
+  }
+  const maxLikes =  Math.max.apply(Math, blogs.map(function(blog){return blog.likes}))
+  const maxBlogObject =  blogs.find(blog => blog.likes === maxLikes)
+  const favouriteBlog = { 'title': maxBlogObject.title,
+    'author':maxBlogObject.author,
+    'likes':maxBlogObject.likes }
+  return favouriteBlog
+
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {
+      'author': '',
+      'blogs': 0
+    }
+  }
+  const blogCounts = {}
+  blogs.forEach(blog => {
+    const author = blog.author
+    Object.prototype.hasOwnProperty.call(blogCounts, author) ? blogCounts[author]++ : blogCounts[author] = 1
+  })
+  const authorWithMostBlogs = Object.keys(blogCounts).reduce((a, b) => blogCounts[a] > blogCounts[b] ? a : b)
+  return {
+    'author': authorWithMostBlogs,
+    'blogs': blogCounts[authorWithMostBlogs]
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return {
+      'author': '',
+      'likes': 0
+    }
+  }
+  const likeCounts = {}
+  blogs.forEach(blog => {
+    const author = blog.author
+    Object.prototype.hasOwnProperty.call(likeCounts, author) ? likeCounts[author]+=blog.likes : likeCounts[author] = blog.likes
+  })
+  const authorWithMostBlogs = Object.keys(likeCounts).reduce((a, b) => likeCounts[a] > likeCounts[b] ? a : b)
+  return {
+    'author': authorWithMostBlogs,
+    'likes': likeCounts[authorWithMostBlogs]
   }
 }
 
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
